01ffb4dce2d220eee169a06e3f70c43435ce12d7
skibidilicious <jackh.224307@gmail.com>
2024-09-08 12:04:35 -0500

    Create middle.js

---
+++ js/middle.js
@@ -0,0 +1,16 @@
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'b') {
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    
+    const middleClickEvent = new MouseEvent('click', {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+      button: 1,
+      clientX: centerX,
+      clientY: centerY
+    });
+    document.elementFromPoint(centerX, centerY).dispatchEvent(middleClickEvent);
+  }
+});
